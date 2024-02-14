65ce91abae3d3cc6343ca047af3206fb04c51065
Dawa Sherpa <dawa.sherpa@esignature.com.np>
2024-02-14 12:41:13 +0545

    onchange
    onchange input testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,69 +1,76 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test("Test for react app", () => {
-  render(<App />);
-  const text = screen.getByText(/First React Test Case/i);
-  const title = screen.getByTitle('ai generated img');
-  expect(text).toBeInTheDocument();
-  expect(title).toBeInTheDocument();
-})
+// test("Test for react app", () => {
+//   render(<App />);
+//   const text = screen.getByText(/First React Test Case/i);
+//   const title = screen.getByTitle('ai generated img');
+//   expect(text).toBeInTheDocument();
+//   expect(title).toBeInTheDocument();
+// })
+// 
+// test("testing input box", () => {
+//   render(<App />);
+//   const input = screen.getByRole("textbox");
+//   const placeholder = screen.getByPlaceholderText('enter user name');
+//   expect(input).toBeInTheDocument();
+//   expect(placeholder).toBeInTheDocument();
+//   expect(input).toHaveAttribute("name", "username");
+//   expect(input).toHaveAttribute("type", "text");
+// })
+// 
+// describe.skip("UI test case group", () => {
+//   test("test case 1", () => {
+//     render(<App />);
+//     let checkInput = screen.getByRole("textbox");
+//     expect(checkInput).toBeInTheDocument();
+// 
+//   })
+//   test("test case 2", () => {
+//     render(<App />);
+//     let checkInput = screen.getByRole("textbox");
+//     expect(checkInput).toBeInTheDocument();
+//   })
+//   test("test case 3", () => {
+//     render(<App />);
+//     let checkInput = screen.getByRole("textbox");
+//     expect(checkInput).toBeInTheDocument();
+// 
+//   })
+// })
+// 
+// describe.only("API test case", () => {
+//   test("api case 1", () => {
+//     render(<App />);
+//     let checkInput = screen.getByRole("textbox");
+//     expect(checkInput).toBeInTheDocument();
+//   });
+//   test("api case 2", () => {
+//     render(<App />);
+//     let checkInput = screen.getByRole("textbox");
+//     expect(checkInput).toBeInTheDocument();
+//   });
+//   test("api case 3", () => {
+//     render(<App />);
+//     let checkInput = screen.getByRole("textbox");
+//     expect(checkInput).toBeInTheDocument();
+// 
+//   })
+// 
+//   describe("inner test group", () => {
+//     test("api inner", () => {
+// 
+//       render(<App />);
+//       const input = screen.getByRole("textbox");
+//       expect(input).toBeInTheDocument();
+//       expect(input).toHaveAttribute("type", "text")
+//     })
+//   })
+// })
 
-test("testing input box", () => {
+test("change event testing", () => {
   render(<App />);
   const input = screen.getByRole("textbox");
-  const placeholder = screen.getByPlaceholderText('enter user name');
-  expect(input).toBeInTheDocument();
-  expect(placeholder).toBeInTheDocument();
-  expect(input).toHaveAttribute("name", "username");
-  expect(input).toHaveAttribute("type", "text");
-})
-
-describe.skip("UI test case group", () => {
-  test("test case 1", () => {
-    render(<App />);
-    let checkInput = screen.getByRole("textbox");
-    expect(checkInput).toBeInTheDocument();
-
-  })
-  test("test case 2", () => {
-    render(<App />);
-    let checkInput = screen.getByRole("textbox");
-    expect(checkInput).toBeInTheDocument();
-  })
-  test("test case 3", () => {
-    render(<App />);
-    let checkInput = screen.getByRole("textbox");
-    expect(checkInput).toBeInTheDocument();
-
-  })
-})
-
-describe.only("API test case", () => {
-  test("api case 1", () => {
-    render(<App />);
-    let checkInput = screen.getByRole("textbox");
-    expect(checkInput).toBeInTheDocument();
-  });
-  test("api case 2", () => {
-    render(<App />);
-    let checkInput = screen.getByRole("textbox");
-    expect(checkInput).toBeInTheDocument();
-  });
-  test("api case 3", () => {
-    render(<App />);
-    let checkInput = screen.getByRole("textbox");
-    expect(checkInput).toBeInTheDocument();
-
-  })
-
-  describe("inner test group", () => {
-    test("api inner", () => {
-
-      render(<App />);
-      const input = screen.getByRole("textbox");
-      expect(input).toBeInTheDocument();
-      expect(input).toHaveAttribute("type", "text")
-    })
-  })
+  fireEvent.change(input, { target: { value: 'a' } });
+  expect(input.value).toBe('a')
 })
