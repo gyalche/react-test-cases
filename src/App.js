b9b0f820b28f90f0719ec3032a5241559e2ae76d
Dawa Sherpa <dawa.sherpa@esignature.com.np>
2024-02-14 12:18:16 +0545

    react-testing describe
    inner describe

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>First React test case</p>
+      <input type="text" placeholder="enter user name" name="username" />
+      <br />
+      <img title="ai generated img" src='https://media.istockphoto.com/id/637696304/photo/patan.jpg?s=612x612&w=0&k=20&c=-53aSTGBGoOOqX5aoC3Hs1jhZ527v3Id_xOawHHVPpg=' />
+    </div>
+  );
+}
+
+export default App;
